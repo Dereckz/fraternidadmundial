fbb4c66b80636294a8ac2fe09fde88499b909e4b
Dereckz <dereckz1304@gmail.com>
2025-01-21 09:22:32 -0600

    revision

--- panel/upload.php
+++ panel/upload.php
@@ -20,6 +20,17 @@
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="css/ruang-admin.min.css" rel="stylesheet">
   <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <!-- Select2 -->
+  <link href="vendor/select2/dist/css/select2.min.css" rel="stylesheet" type="text/css">
+  <!-- Bootstrap DatePicker -->  
+  <link href="vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet" >
+  <!-- Bootstrap Touchspin -->
+  <link href="vendor/bootstrap-touchspin/css/jquery.bootstrap-touchspin.css" rel="stylesheet" >
+  <!-- ClockPicker -->
+  <link href="vendor/clock-picker/clockpicker.css" rel="stylesheet">
+ 
 </head>
 
 <body id="page-top">
@@ -127,8 +138,8 @@
           <div class="row">
             <!-- Datatables -->
             <div class="col-lg-12">
-  <!-- Form Basic -->
-  <div class="card mb-4">
+      <!-- Form Basic -->
+      <div class="card mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Form Basic</h6>
                 </div>
@@ -147,14 +158,13 @@
                         <label class="custom-file-label" for="customFile">Elija el artículo.</label>
                       </div>
                     </div>
-
                     <div class="form-group" id="simple-date1">
-                    <label for="simpleDataInput">Simple Data Input</label>
+                    <label for="simpleDataInput">Fecha de artículo</label>
                       <div class="input-group date">
                         <div class="input-group-prepend">
                           <span class="input-group-text"><i class="fas fa-calendar"></i></span>
                         </div>
-                        <input type="text" class="form-control" value="01/06/2020" id="simpleDataInput">
+                        <input type="text" class="form-control" value="<?php echo date("d/m/Y");?>" id="simpleDataInput">
                       </div>
                   </div>
                   
@@ -218,11 +228,12 @@
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
             <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
-              <b><a href="https://indrijunanda.gitlab.io/" target="_blank">indrijunanda</a></b>
+              <b><a href="https://desetecnologias.net//" target="_blank">Dësë Tecnologías</a></b>
             </span>
           </div>
         </div>
       </footer>
+     
       <!-- Footer -->
     </div>
   </div>
@@ -240,15 +251,122 @@
   <!-- Page level plugins -->
   <script src="vendor/datatables/jquery.dataTables.min.js"></script>
   <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <!-- Select2 -->
+  <script src="vendor/select2/dist/js/select2.min.js"></script>
+  <!-- Bootstrap Datepicker -->
+  <script src="vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+  <!-- Bootstrap Touchspin -->
+  <script src="vendor/bootstrap-touchspin/js/jquery.bootstrap-touchspin.js"></script>
+  <!-- ClockPicker -->
+  <script src="vendor/clock-picker/clockpicker.js"></script>
+  <!-- RuangAdmin Javascript -->
+  <script src="js/ruang-admin.min.js"></script>
+  <!-- Javascript for this page -->
+  
+ <script>
 
-  <!-- Page level custom scripts -->
-  <script>
     $(document).ready(function () {
-      $('#dataTable').DataTable(); // ID From dataTable 
-      $('#dataTableHover').DataTable(); // ID From dataTable with Hover
-    });
-  </script>
+        $('#dataTable').DataTable(); // ID From dataTable 
+        $('#dataTableHover').DataTable(); // ID From dataTable with Hover
+      });
 
+  $(document).ready(function () {
+
+
+    $('.select2-single').select2();
+
+    // Select2 Single  with Placeholder
+    $('.select2-single-placeholder').select2({
+      placeholder: "Select a Province",
+      allowClear: true
+    });      
+
+    // Select2 Multiple
+    $('.select2-multiple').select2();
+
+    // Bootstrap Date Picker
+    $('#simple-date1 .input-group.date').datepicker({
+      format: 'dd/mm/yyyy',
+      todayBtn: 'linked',
+      todayHighlight: true,
+      autoclose: true,        
+    });
+
+    $('#simple-date2 .input-group.date').datepicker({
+      startView: 1,
+      format: 'dd/mm/yyyy',        
+      autoclose: true,     
+      todayHighlight: true,   
+      todayBtn: 'linked',
+    });
+
+    $('#simple-date3 .input-group.date').datepicker({
+      startView: 2,
+      format: 'dd/mm/yyyy',        
+      autoclose: true,     
+      todayHighlight: true,   
+      todayBtn: 'linked',
+    });
+
+    $('#simple-date4 .input-daterange').datepicker({        
+      format: 'dd/mm/yyyy',        
+      autoclose: true,     
+      todayHighlight: true,   
+      todayBtn: 'linked',
+    });    
+
+    // TouchSpin
+
+    $('#touchSpin1').TouchSpin({
+      min: 0,
+      max: 100,                
+      boostat: 5,
+      maxboostedstep: 10,        
+      initval: 0
+    });
+
+    $('#touchSpin2').TouchSpin({
+      min:0,
+      max: 100,
+      decimals: 2,
+      step: 0.1,
+      postfix: '%',
+      initval: 0,
+      boostat: 5,
+      maxboostedstep: 10
+    });
+
+    $('#touchSpin3').TouchSpin({
+      min: 0,
+      max: 100,
+      initval: 0,
+      boostat: 5,
+      maxboostedstep: 10,
+      verticalbuttons: true,
+    });
+
+    $('#clockPicker1').clockpicker({
+      donetext: 'Done'
+    });
+
+    $('#clockPicker2').clockpicker({
+      autoclose: true
+    });
+
+    let input = $('#clockPicker3').clockpicker({
+      autoclose: true,
+      'default': 'now',
+      placement: 'top',
+      align: 'left',
+    });
+
+    $('#check-minutes').click(function(e){        
+      e.stopPropagation();
+      input.clockpicker('show').clockpicker('toggleView', 'minutes');
+    });
+
+  });
+</script>
 </body>
 
 </html>
