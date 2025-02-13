ff1845c4ff267a2c74ff18fe3134bb184e868063
Dereckz <dereckz1304@gmail.com>
2025-02-13 12:08:21 -0600

    beit

--- index.php
+++ index.php
@@ -329,10 +329,30 @@
                                 </div>
                             </div>
                         </div>
-                        <!--Ventana Modal para Agregar--->
-                        <?php include('ModalAgregar.php'); ?>
+                        
+                        
                         <?php  }?>
-                       
+                        <!-- Modal-->
+                        <div class="modal fade" id="modalPdf" tabindex="-1" aria-labelledby="modalPdf" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Ver archivo</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <iframe id="iframePDF" frameborder="0" scrolling="no" width="100%" height="500px"></iframe>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin modal -->
                     </div>
                     <!----------------------------------- FIN DE CARD PRO MODAL CV--------------------->
                    
@@ -527,14 +547,36 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        
+       <script>
+            function onSubmitForm() {
+                                var frm = document.getElementById('form1');
+                                var data = new FormData(frm);
+                                var xhttp = new XMLHttpRequest();
+                                xhttp.onreadystatechange = function () {
+                                    if (this.readyState == 4) {
+                                        var msg = xhttp.responseText;
+                                        if (msg == 'success') {
+                                            alert(msg);
+                                            $('#exampleModal').modal('hide')
+                                        } else {
+                                            alert(msg);
+                                        }
+
+                                    }
+                                };
+                                xhttp.open("POST", "upload.php", true);
+                                xhttp.send(data);
+                                $('#form1').trigger('reset');
+                            }
+        function openModelPDF(url) {
+                $('#modalPdf').modal('show');
+                $('#iframePDF').attr('src','<?php echo 'http://localhost/fraternidadmundial.com'; ?>'+url);
+            }
+       </script> 
 <script>
     AOS.init();
 
-        function openModelPDF(url) {
-                $('#modalPdf').modal('show');
-                $('#iframePDF').attr('src','<?php echo 'http://localhost/fraternidadmundial.com/'; ?>'+url);
-            }
+        
 </script>
 
 </html>
